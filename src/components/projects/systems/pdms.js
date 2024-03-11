019e1398e56ddefa60bef626634b6c8476e9e8e0
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-03-11 22:36:57 +0800

    add snapshots with tumbnail in pd projects

---
+++ src/components/projects/systems/pdms.js
@@ -0,0 +1,43 @@
+export const PhotoService = {
+    getData() {
+      return [
+        {
+          itemImageSrc:
+            "pd/pd1.jpeg",
+          thumbnailImageSrc:
+            "pd/pd1-thumb.jpeg",
+          alt: "The login page of the system",
+          title: "Login Page",
+        },
+        {
+          itemImageSrc:
+            "pd/pd2.jpeg",
+          thumbnailImageSrc:
+            "pd/pd2-thumb.jpeg",
+          alt: "The landing page of the system after login.",
+          title: "Dashboard",
+        },
+        {
+            itemImageSrc:
+            "pd/pd3.jpeg",
+          thumbnailImageSrc:
+            "pd/pd3-thumb.jpeg",
+            "alt": "This is the main module of the system for creating target deliverables and submitting them monthly.",
+            "title": "Operations Review"
+        },
+        {
+           itemImageSrc:
+            "pd/pd4.jpeg",
+          thumbnailImageSrc:
+            "pd/pd4-thumb.jpeg",
+            "alt": "This module displays the monitoring of accomplishments for processes, divided into core and support processes. It follows a color scheme with red indicating not started/stopped, yellow for ongoing, and green for accomplished.",
+            "title": "Graphs Representation"
+        },
+      ];
+    },
+  
+    getImages() {
+      return Promise.resolve(this.getData());
+    },
+  };
+  
