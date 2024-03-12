0c68f536e2b63e3ed1d25f6fbf4f90333525d2de
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-03-12 23:06:04 +0800

    CHD snapshots

--- src/components/projects/systems/cdh.js
+++ src/components/projects/systems/cdh.js
@@ -30,9 +30,58 @@ export const CdService = {
             "cd/cd4.png",
           thumbnailImageSrc:
             "cd/cd4-thumb.png",
-            "alt": "This module displays the monitoring of accomplishments for processes, divided into core and support processes. It follows a color scheme with red indicating not started/stopped, yellow for ongoing, and green for accomplished.",
+            "alt": "Presents the complete consultation history of a patient, enabling you to view both past and present consultations.",
             "title": "Patient Consultation History"
         },
+        {
+            itemImageSrc:
+             "cd/cd6.png",
+           thumbnailImageSrc:
+             "cd/cd6-thumb.png",
+             "alt": "Facilitates the systematic recording of patient data upon consultation.",
+             "title": "Patient Chart Registration"
+         },
+         {
+            itemImageSrc:
+             "cd/cd5.png",
+           thumbnailImageSrc:
+             "cd/cd5-thumb.png",
+             "alt": "Users must complete the chart section requirements before proceeding to saving.",
+             "title": "Patient Chart Registration Validation"
+         },
+         {
+            itemImageSrc:
+             "cd/cd7.png",
+           thumbnailImageSrc:
+             "cd/cd7-thumb.png",
+             "alt": "Vital Signs",
+             "title": "Patient Chart Sections"
+         },
+    
+         {
+            itemImageSrc:
+             "cd/cd9.png",
+           thumbnailImageSrc:
+             "cd/cd9-thumb.png",
+             "alt": "Past History and Family History",
+             "title": "Patient Chart Sections"
+         },
+         {
+            itemImageSrc:
+             "cd/cd10.png",
+           thumbnailImageSrc:
+             "cd/cd10-thumb.png",
+             "alt": "Vaccination and SOAP (Subjective, Objective, Assessment and Plan)",
+             "title": "Patient Chart Sections"
+         },
+         {
+            itemImageSrc:
+             "cd/cd11.png",
+           thumbnailImageSrc:
+             "cd/cd11-thumb.png",
+             "alt": "Users can generate a PDF report by selecting a consultation date range",
+             "title": "Report Generation"
+         },
       ];
     },
   
