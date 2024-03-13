ff0f313609a4ec627844ef73ebef70b6ca4e3890
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-03-13 23:35:47 +0800

    Add 5 new items to the VPD module list: Patient Disease Registration, Skeleton Loader, Linelist Creation, User Management, and User Management - Dark Mode.

--- src/components/projects/systems/vpd.js
+++ src/components/projects/systems/vpd.js
@@ -25,6 +25,36 @@ export const VpService = {
         alt: "This module displays a list of currently registered patient profiles.",
         title: "Patient Profile List",
       },
+      {
+        itemImageSrc: "vp/vp6.png",
+        thumbnailImageSrc: "vp/vp6-thumb.png",
+        alt: "Systematic registration of patients' disease medical record.",
+        title: "Patient Disease Registration",
+      },
+      {
+        itemImageSrc: "vp/vp5.png",
+        thumbnailImageSrc: "vp/vp5-thumb.png",
+        alt: "Custom skeleton loader component displayed while waiting for data from the backend API.",
+        title: "Skeleton Loader"
+      },
+      {
+        itemImageSrc: "vp/vp7.png",
+        thumbnailImageSrc: "vp/vp7-thumb.png",
+        alt: "Module for creating a linelist by selecting multiple patients, similar to an 'add to cart' functionality",
+        title: "Linelist Creation"
+      },
+    {
+      itemImageSrc: "vp/vp8.png",
+      thumbnailImageSrc: "vp/vp8-thumb.png",
+      alt: "Module used for creating new user data, assigning roles, and uploading photos and signatures.",
+      title: "User Management"
+    },
+      {
+      itemImageSrc: "vp/vp9.png",
+      thumbnailImageSrc: "vp/vp9-thumb.png",
+      alt: "User Management in dark mode.",
+      title: "User Management - Dark Mode"
+    }
     ];
   },
 
