3cb8c8969b2f390a18418bad9b7d81a8a6578506
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-03-13 11:34:47 +0800

    update about page

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -1,43 +1,51 @@
 <template>
   <section class="about" :id="sectionId">
-    <div class="animation-duration-1000" v-animateonscroll="{ enterClass: 'fadein', leaveClass: 'fadeout' }">
-
-    <h2 class="heading animation-duration-1000">About</h2>
-    <div class="about-content">
-      <div v-animateonscroll="{ enterClass: 'fadein', leaveClass: 'fadeout' }"
-        class="about-paragraph animation-duration-1000">
-        <p>
-          Hello, I'm Joe, a passionate software engineer with
-          4+ years of industry experience.
-        </p>
-        <p>
-          My passion is to build functional web applications using the skills I
-          have self-studied and learned through mentorship.
-        </p>
-        <p>
-          My interest in web development started when I was a graduating student
-          in junior high school. Whenever I visited a website, I was curious
-          about the process behind the user's view, so I chose to study
-          Information Technology in college to learn more about it. That's where
-          my passion for web development began. Over the years, I've refined my
-          skills, diving into coding, design, and user experience to create
-          dynamic, functional, user-friendly web applications. I remain
-          dedicated to continually expanding my knowledge in this dynamic field.
-        </p>
-        <p>
-          What I like most about the industry I'm in is its continuous evolution. Technology continually
-          challenges me to learn and stay updated on new technologies and frameworks as they release new versions to
-          enhance their performance and fix ongoing issues. This nature of techonolgy evolution not only keeps me
-          interested in this field but also inspires my passion for facing new challenges.
-        </p>
+    <div
+      class="animation-duration-1000"
+      v-animateonscroll="{ enterClass: 'fadein', leaveClass: 'fadeout' }"
+    >
+      <h2 class="heading animation-duration-1000">About</h2>
+      <div class="about-content">
+        <div
+          v-animateonscroll="{ enterClass: 'fadein', leaveClass: 'fadeout' }"
+          class="about-paragraph animation-duration-1000"
+        >
+          <p>
+            Hello, I'm Joe, a passionate software engineer with 4+ years of
+            industry experience.
+          </p>
+          <p>
+            My passion is to build functional web applications using the skills
+            I have self-studied and learned through mentorship.
+          </p>
+          <p>
+            My interest in web development started when I was a graduating
+            student in junior high school. Whenever I visited a website, I was
+            curious about the process behind the user's view, so I chose to
+            study Information Technology in college to learn more about it.
+            That's where my passion for web development began. Over the years,
+            I've refined my skills, diving into coding, design, and user
+            experience to create dynamic, functional, user-friendly web
+            applications. I remain dedicated to continually expanding my
+            knowledge in this dynamic field.
+          </p>
+          <p>
+            What I like most about the industry I'm in is its continuous
+            evolution. Technology continually challenges me to learn and stay
+            updated on new technologies and frameworks as they release new
+            versions to enhance their performance and fix ongoing issues. This
+            nature of techonolgy evolution not only keeps me interested in this
+            field but also inspires my passion for facing new challenges.
+          </p>
+        </div>
+        <div
+          v-animateonscroll="{ enterClass: 'fadein', leaveClass: 'fadeout' }"
+          class="about-paragraph animation-duration-1000"
+        >
+          <h3>Bachelor of Science in Information Technology</h3>
+          <span>Don Honorio Ventura Technological State University</span>
+        </div>
       </div>
-      <div v-animateonscroll="{ enterClass: 'fadein', leaveClass: 'fadeout' }"
-        class="about-paragraph animation-duration-1000">
-        <h3>Bachelor of Science in Information Technology</h3>
-        <span>Don Honorio Ventura State University</span>
-      </div>
-    </div>
-          
     </div>
   </section>
 </template>
